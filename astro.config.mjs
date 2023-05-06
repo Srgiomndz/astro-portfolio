c4459b1bffe9636e764af439170db23b2338700c
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-05-06 15:41:42 -0700

    added google analytics using partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
+
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      
-    }
-  }), react()],
+  integrations: [
+    tailwind({
+      config: {},
+    }),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    }),
+  ],
 })
